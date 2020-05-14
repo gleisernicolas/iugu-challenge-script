91dc89ab1eeb4fa4c79a7d54e5af374a1438d722
Nicolas Gleiser <ncs.cr4sh@gmail.com>
2020-05-14 16:34:57 -0300

    fist commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## ruby-transaction-script
+Por ser um projeto simples eu preferi não usar nenhuma gem ou framework pra execução dele, apenas usei bibliotecas padrão ruby.
+
+## Execução
+Para executar o projeto basta executar o seguinte dentro da pasta do projeto `ruby challenge.rb ./path/to/account.csv ./path/to/transactions.csv`
---
+++ challenge.rb
@@ -0,0 +1,37 @@
+abort("wrong number of arguments, expected 2 got #{ARGV.count}") if ARGV.count != 2
+
+def transform_array(array)
+  arr = array.map do |string|
+    values = string.gsub('\n', '').split(',')
+    values = [values.first, values.last.to_i]
+  end
+
+  Hash[*arr.flatten]
+end
+
+def complete_transaction(account_number, value)
+  result = @accounts[account_number] + value
+  result -= 300 if result < 0
+
+  @accounts[account_number] = result
+
+  @accounts
+end
+
+
+@accounts = transform_array(File.readlines(ARGV[0]))
+transactions = File.readlines(ARGV[1]).map do |line|
+  line.gsub('\n', '').split(',').map(&:to_i)
+end
+
+transactions.each do |transaction|
+  complete_transaction(transaction[0].to_s, transaction[1])
+end
+
+
+
+@accounts.each do |key, value|
+  print "#{key},#{value}\n"
+end
+
+exit
