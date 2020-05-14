e0db864c79e8acb1bd7f82496c5681a8328f1bc1
Nicolas Gleiser <ncs.cr4sh@gmail.com>
2020-05-14 17:56:19 -0300

    add comments

--- challenge.rb
+++ challenge.rb
@@ -1,17 +1,23 @@
 abort("wrong number of arguments, expected 2 got #{ARGV.count}") if ARGV.count != 2
+FINE = 300
 
+# Nesse metodo eu transformo as linhas do arquivo em um hash onde a chave é o número da conta e o valor é o saldo inicial,
+# foi feito desta forma pra evitar o uso de um banco de dados 
+# output {"8983" => 99999, "9932" => 7787}
 def transform_array(array)
-  arr = array.map do |string|
+  arrays = array.map do |string|
     values = string.gsub('\n', '').split(',')
     values = [values.first, values.last.to_i]
   end
 
-  Hash[*arr.flatten]
+  Hash[*arrays.flatten]
 end
 
+# Aqui eu é onde eu faço a transação, sempre somando pois a entrada de um débito é um numero negativo
+# Também é adicionado a multa ao resultado, sempre quando ele for menor que 0
 def complete_transaction(account_number, value)
   result = @accounts[account_number] + value
-  result -= 300 if result < 0
+  result -= FINE if result < 0
 
   @accounts[account_number] = result
 end
